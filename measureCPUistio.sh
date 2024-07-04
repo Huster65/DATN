0ad572313df731a4096b36106339b711d307eb72
Huster65 <124910987+Huster65@users.noreply.github.com>
2024-07-04 21:58:49 +0700

    Update measureCPUistio.sh

--- measureCPUistio.sh
+++ measureCPUistio.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Đặt tên cho pod và container
 POD_NAME="my-pod"
 CONTAINER_NAME="my-container"
 
@@ -13,16 +14,21 @@ SAMPLE_COUNT=0
 TOTAL_CPU=0
 
 while [ $SAMPLE_COUNT -lt $SAMPLE_SECONDS ]; do
+    # Lấy thông tin sử dụng tài nguyên của container
     TOP_OUTPUT=$(kubectl top pod $POD_NAME --containers)
-    CPU_USAGE=$(echo "$TOP_OUTPUT" | grep $CONTAINER_NAME | awk '{print $2}')
+    
+    # Lọc ra dòng chứa tên container và lấy giá trị sử dụng CPU
+    CPU_USAGE=$(echo "$TOP_OUTPUT" | grep $CONTAINER_NAME | awk '{print $3}' | sed 's/m//')
 
+    # Cộng giá trị sử dụng CPU của lần mẫu hiện tại vào tổng CPU
     TOTAL_CPU=$(echo "$TOTAL_CPU + $CPU_USAGE" | bc)
 
+    # Tăng giá trị của SAMPLE_COUNT lên 1 và dừng 1 giây trước khi thực hiện lần mẫu tiếp theo
     SAMPLE_COUNT=$((SAMPLE_COUNT + 1))
     sleep 1
 done
 
+# Tính giá trị trung bình của sử dụng CPU
 AVG_CPU=$(echo "scale=2; $TOTAL_CPU / $SAMPLE_SECONDS" | bc)
-CPU_PERCENT=$(echo "scale=2; $AVG_CPU * 100 / 1" | bc)
 
-echo "Average CPU usage of container $CONTAINER_NAME in pod $POD_NAME over $SAMPLE_SECONDS seconds: $CPU_PERCENT %"
+echo "Average CPU usage of container $CONTAINER_NAME in pod $POD_NAME over $SAMPLE_SECONDS seconds: $AVG_CPU mCPU"
